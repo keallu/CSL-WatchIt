38366274f9d3c0eb737289d5707e532ab087b14f
Keallu <45124091+keallu@users.noreply.github.com>
2022-01-12 08:57:41 +0100

    Version 1.11.

--- WatchIt/LimitItem.cs
+++ WatchIt/LimitItem.cs
@@ -3,6 +3,7 @@ using ColossalFramework.UI;
 using System;
 using System.Globalization;
 using UnityEngine;
+using EManagersLib.API;
 
 namespace WatchIt
 {
@@ -57,7 +58,7 @@ namespace WatchIt
                 _amount.autoSize = false;
                 _amount.height = 23f;
                 _amount.width = 150f;
-                _amount.relativePosition = new Vector3(165f, 2f);                
+                _amount.relativePosition = new Vector3(165f, 2f);
 
                 _capacity = UIUtils.CreateLabel(_panel, "LimitCapacity", "0");
                 _capacity.textScale = 0.875f;
@@ -145,144 +146,165 @@ namespace WatchIt
 
         private void GetAmountAndCapacity(string name, out int amount, out int capacity, out bool modded)
         {
-            CitizenManager citizenManager;
-            NetManager netManager;
-            AudioManager audioManager;
-            VehicleManager vehicleManager;
-
-            switch (name)
+            try
             {
-                case "Areas":
-                    GameAreaManager gameAreaManager = Singleton<GameAreaManager>.instance;
-                    amount = gameAreaManager.m_areaCount;
-                    capacity = gameAreaManager.m_maxAreaCount;
-                    modded = gameAreaManager.m_maxAreaCount > 9 ? true : false;
-                    break;
-                case "Buildings":
-                    BuildingManager buildingManager = Singleton<BuildingManager>.instance;
-                    amount = buildingManager.m_buildingCount;
-                    capacity = BuildingManager.MAX_BUILDING_COUNT;
-                    modded = false;
-                    break;
-                case "Citizens":
-                    citizenManager = Singleton<CitizenManager>.instance;
-                    amount = citizenManager.m_citizenCount;
-                    capacity = CitizenManager.MAX_CITIZEN_COUNT;
-                    modded = false;
-                    break;
-                case "Citizen Units":
-                    citizenManager = Singleton<CitizenManager>.instance;
-                    amount = citizenManager.m_unitCount;
-                    capacity = CitizenManager.MAX_UNIT_COUNT;
-                    modded = false;
-                    break;
-                case "Citizen Instances":
-                    citizenManager = Singleton<CitizenManager>.instance;
-                    amount = citizenManager.m_instanceCount;
-                    capacity = CitizenManager.MAX_INSTANCE_COUNT;
-                    modded = false;
-                    break;
-                case "Disasters":
-                    DisasterManager disasterManager = Singleton<DisasterManager>.instance;
-                    amount = disasterManager.m_disasterCount;
-                    capacity = DisasterManager.MAX_DISASTER_COUNT;
-                    modded = false;
-                    break;
-                case "Districts":
-                    DistrictManager districtManager = Singleton<DistrictManager>.instance;
-                    amount = districtManager.m_districtCount;
-                    capacity = DistrictManager.MAX_DISTRICT_COUNT;
-                    modded = false;
-                    break;
-                case "Events":
-                    EventManager eventManager = Singleton<EventManager>.instance;
-                    amount = eventManager.m_eventCount;
-                    capacity = EventManager.MAX_EVENT_COUNT;
-                    modded = false;
-                    break;
-                case "Loans":
-                    EconomyManager economyManager = Singleton<EconomyManager>.instance;
-                    amount = economyManager.CountLoans();
-                    capacity = EconomyManager.MAX_LOANS;
-                    modded = false;
-                    break;
-                case "Net Segments":
-                    netManager = Singleton<NetManager>.instance;
-                    amount = netManager.m_segmentCount;
-                    capacity = NetManager.MAX_SEGMENT_COUNT;
-                    modded = false;
-                    break;
-                case "Net Nodes":
-                    netManager = Singleton<NetManager>.instance;
-                    amount = netManager.m_nodeCount;
-                    capacity = NetManager.MAX_NODE_COUNT;
-                    modded = false;
-                    break;
-                case "Net Lanes":
-                    netManager = Singleton<NetManager>.instance;
-                    amount = netManager.m_laneCount;
-                    capacity = NetManager.MAX_LANE_COUNT;
-                    modded = false;
-                    break;
-                case "Path Units":
-                    PathManager pathManager = Singleton<PathManager>.instance;
-                    amount = pathManager.m_pathUnitCount;
-                    capacity = PathManager.MAX_PATHUNIT_COUNT;
-                    modded = false;
-                    break;
-                case "Props":
-                    PropManager propManager = Singleton<PropManager>.instance;
-                    amount = propManager.m_propCount;
-                    capacity = PropManager.MAX_PROP_COUNT;
-                    modded = false;
-                    break;
-                case "Radio Channels":
-                    audioManager = Singleton<AudioManager>.instance;
-                    amount = audioManager.m_radioChannelCount;
-                    capacity = AudioManager.MAX_RADIO_CHANNEL_COUNT;
-                    modded = false;
-                    break;
-                case "Radio Contents":
-                    audioManager = Singleton<AudioManager>.instance;
-                    amount = audioManager.m_radioContentCount;
-                    capacity = AudioManager.MAX_RADIO_CONTENT_COUNT;
-                    modded = false;
-                    break;
-                case "Transport Lines":
-                    TransportManager transportManager = Singleton<TransportManager>.instance;
-                    amount = transportManager.m_lineCount;
-                    capacity = TransportManager.MAX_LINE_COUNT;
-                    modded = false;
-                    break;
-                case "Trees":
-                    TreeManager treeManager = Singleton<TreeManager>.instance;
-                    amount = treeManager.m_treeCount;
-                    capacity = treeManager.m_trees.m_size > 262144 ? (int)treeManager.m_trees.m_size : TreeManager.MAX_TREE_COUNT;
-                    modded = treeManager.m_trees.m_size > 262144 ? true : false;
-                    break;
-                case "Vehicles":
-                    vehicleManager = Singleton<VehicleManager>.instance;
-                    amount = vehicleManager.m_vehicleCount;
-                    capacity = VehicleManager.MAX_VEHICLE_COUNT;
-                    modded = false;
-                    break;
-                case "Vehicles Parked":
-                    vehicleManager = Singleton<VehicleManager>.instance;
-                    amount = vehicleManager.m_parkedCount;
-                    capacity = VehicleManager.MAX_PARKED_COUNT;
-                    modded = false;
-                    break;
-                case "Zoned Blocks":
-                    ZoneManager zoneManager = Singleton<ZoneManager>.instance;
-                    amount = zoneManager.m_blockCount;
-                    capacity = ZoneManager.MAX_BLOCK_COUNT;
-                    modded = false;
-                    break;
-                default:
-                    amount = 0;
-                    capacity = 0;
-                    modded = false;
-                    break;
+                CitizenManager citizenManager;
+                NetManager netManager;
+                AudioManager audioManager;
+                VehicleManager vehicleManager;
+
+                switch (name)
+                {
+                    case "Areas":
+                        GameAreaManager gameAreaManager = Singleton<GameAreaManager>.instance;
+                        amount = gameAreaManager.m_areaCount;
+                        capacity = gameAreaManager.m_maxAreaCount;
+                        modded = gameAreaManager.m_maxAreaCount > 9;
+                        break;
+                    case "Buildings":
+                        BuildingManager buildingManager = Singleton<BuildingManager>.instance;
+                        amount = buildingManager.m_buildingCount;
+                        capacity = (int)buildingManager.m_buildings.m_size;
+                        modded = buildingManager.m_buildings.m_size > BuildingManager.MAX_BUILDING_COUNT;
+                        break;
+                    case "Citizens":
+                        citizenManager = Singleton<CitizenManager>.instance;
+                        amount = citizenManager.m_citizenCount;
+                        capacity = (int)citizenManager.m_citizens.m_size;
+                        modded = citizenManager.m_citizens.m_size > CitizenManager.MAX_CITIZEN_COUNT;
+                        break;
+                    case "Citizen Units":
+                        citizenManager = Singleton<CitizenManager>.instance;
+                        amount = citizenManager.m_unitCount;
+                        capacity = (int)citizenManager.m_units.m_size;
+                        modded = citizenManager.m_units.m_size > CitizenManager.MAX_UNIT_COUNT;
+                        break;
+                    case "Citizen Instances":
+                        citizenManager = Singleton<CitizenManager>.instance;
+                        amount = citizenManager.m_instanceCount;
+                        capacity = (int)citizenManager.m_instances.m_size;
+                        modded = citizenManager.m_instances.m_size > CitizenManager.MAX_INSTANCE_COUNT;
+                        break;
+                    case "Disasters":
+                        DisasterManager disasterManager = Singleton<DisasterManager>.instance;
+                        amount = disasterManager.m_disasterCount;
+                        capacity = disasterManager.m_disasters.m_size;
+                        modded = disasterManager.m_disasters.m_size > DisasterManager.MAX_DISASTER_COUNT;
+                        break;
+                    case "Districts":
+                        DistrictManager districtManager = Singleton<DistrictManager>.instance;
+                        amount = districtManager.m_districtCount;
+                        capacity = (int)districtManager.m_districts.m_size;
+                        modded = districtManager.m_districts.m_size > DistrictManager.MAX_DISTRICT_COUNT;
+                        break;
+                    case "Events":
+                        EventManager eventManager = Singleton<EventManager>.instance;
+                        amount = eventManager.m_eventCount;
+                        capacity = eventManager.m_events.m_size;
+                        modded = eventManager.m_events.m_size > EventManager.MAX_EVENT_COUNT;
+                        break;
+                    case "Loans":
+                        EconomyManager economyManager = Singleton<EconomyManager>.instance;
+                        amount = economyManager.CountLoans();
+                        capacity = EconomyManager.MAX_LOANS;
+                        modded = false;
+                        break;
+                    case "Net Segments":
+                        netManager = Singleton<NetManager>.instance;
+                        amount = netManager.m_segmentCount;
+                        capacity = (int)netManager.m_segments.m_size;
+                        modded = netManager.m_segments.m_size > NetManager.MAX_SEGMENT_COUNT;
+                        break;
+                    case "Net Nodes":
+                        netManager = Singleton<NetManager>.instance;
+                        amount = netManager.m_nodeCount;
+                        capacity = (int)netManager.m_nodes.m_size;
+                        modded = netManager.m_nodes.m_size > NetManager.MAX_NODE_COUNT;
+                        break;
+                    case "Net Lanes":
+                        netManager = Singleton<NetManager>.instance;
+                        amount = netManager.m_laneCount;
+                        capacity = (int)netManager.m_lanes.m_size;
+                        modded = netManager.m_lanes.m_size > NetManager.MAX_LANE_COUNT;
+                        break;
+                    case "Path Units":
+                        PathManager pathManager = Singleton<PathManager>.instance;
+                        amount = pathManager.m_pathUnitCount;
+                        capacity = (int)pathManager.m_pathUnits.m_size;
+                        modded = pathManager.m_pathUnits.m_size > PathManager.MAX_PATHUNIT_COUNT;
+                        break;
+                    case "Props":
+                        PropManager propManager = Singleton<PropManager>.instance;
+                        amount = propManager.m_propCount;
+
+                        if (propManager.m_props == null && PropAPI.m_isEMLInstalled)
+                        {
+                            capacity = PropAPI.PropBufferLen;
+                            modded = true;
+                        }
+                        else
+                        {
+                            capacity = (int)propManager.m_props.m_size;
+                            modded = false;
+                        }                        
+
+                        break;
+                    case "Radio Channels":
+                        audioManager = Singleton<AudioManager>.instance;
+                        amount = audioManager.m_radioChannelCount;
+                        capacity = audioManager.m_radioChannels.m_size;
+                        modded = audioManager.m_radioChannels.m_size > AudioManager.MAX_RADIO_CHANNEL_COUNT;
+                        break;
+                    case "Radio Contents":
+                        audioManager = Singleton<AudioManager>.instance;
+                        amount = audioManager.m_radioContentCount;
+                        capacity = audioManager.m_radioContents.m_size;
+                        modded = audioManager.m_radioContents.m_size > AudioManager.MAX_RADIO_CONTENT_COUNT;
+                        break;
+                    case "Transport Lines":
+                        TransportManager transportManager = Singleton<TransportManager>.instance;
+                        amount = transportManager.m_lineCount;
+                        capacity = (int)transportManager.m_lines.m_size;
+                        modded = transportManager.m_lines.m_size > TransportManager.MAX_LINE_COUNT;
+                        break;
+                    case "Trees":
+                        TreeManager treeManager = Singleton<TreeManager>.instance;
+                        amount = treeManager.m_treeCount;
+                        capacity = (int)treeManager.m_trees.m_size;
+                        modded = treeManager.m_trees.m_size > TreeManager.MAX_TREE_COUNT;
+                        break;
+                    case "Vehicles":
+                        vehicleManager = Singleton<VehicleManager>.instance;
+                        amount = vehicleManager.m_vehicleCount;
+                        capacity = (int)vehicleManager.m_vehicles.m_size;
+                        modded = vehicleManager.m_vehicles.m_size > VehicleManager.MAX_VEHICLE_COUNT;
+                        break;
+                    case "Vehicles Parked":
+                        vehicleManager = Singleton<VehicleManager>.instance;
+                        amount = vehicleManager.m_parkedCount;
+                        capacity = (int)vehicleManager.m_parkedVehicles.m_size;
+                        modded = vehicleManager.m_parkedVehicles.m_size > VehicleManager.MAX_PARKED_COUNT;
+                        break;
+                    case "Zoned Blocks":
+                        ZoneManager zoneManager = Singleton<ZoneManager>.instance;
+                        amount = zoneManager.m_blockCount;
+                        capacity = (int)zoneManager.m_blocks.m_size;
+                        modded = zoneManager.m_blocks.m_size > ZoneManager.MAX_BLOCK_COUNT;
+                        break;
+                    default:
+                        amount = 0;
+                        capacity = 0;
+                        modded = false;
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log("[Watch It!] LimitItem:GetAmountAndCapacity -> Exception: " + e.Message);
+
+                amount = 0;
+                capacity = 0;
+                modded = false;
             }
         }
 
